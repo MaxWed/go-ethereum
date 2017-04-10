d0f968d36e757585ca34770502cf8a4370b805bf
MaxWed <maxtrader.max@gmail.com>
2017-04-10 18:00:36 +0100

    Update config.go

--- params/config.go
+++ params/config.go
@@ -42,7 +42,7 @@ var TestnetChainConfig = &ChainConfig{
 	DAOForkBlock:   nil,
 	DAOForkSupport: true,
 	EIP150Block:    big.NewInt(0),
-	EIP150Hash:     common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d"),
+	EIP150Hash:     common.HexToHash(),// "0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d"
 	EIP155Block:    big.NewInt(10),
 	EIP158Block:    big.NewInt(10),
 }
